d7d1f3095c3df7e2079a41cbf211a3ea81d356fc
joaomorito <joaomorito04@gmail.com>
2025-05-06 14:21:44 -0300

    Entidade Client criada

---
+++ src/main/java/com/example/loginauthapi/domain/user/Client.java
@@ -0,0 +1,23 @@
+package com.example.loginauthapi.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "client")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String cpf;
+}
